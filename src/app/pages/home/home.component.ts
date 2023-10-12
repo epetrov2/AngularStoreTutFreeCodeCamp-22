70028458489f2edd89287f1886447f59a069950d
Viktor Lachkov <EPetrov2@gmail.com>
2023-10-12 16:35:24 +0400

    Add AddToCart service to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
-const ROWS_HEIGHT: { [id: number]: number } = {1: 400, 3: 335, 4: 350};
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
 @Component({
   selector: 'app-home',
@@ -10,16 +12,27 @@ export class HomeComponent {
 
   cols = 3;
   category: string | undefined;
-  rowHeight: number =  ROWS_HEIGHT[this.cols];
+  rowHeight: number = ROWS_HEIGHT[this.cols];
 
+  constructor(private cartService: CartService) { }
 
   onColumnsCountChange(newColsNumber: number): void {
     this.cols = newColsNumber;
-    this.rowHeight =  ROWS_HEIGHT[this.cols];
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+  }
+
+  onAddToCart(newProduct: Product): void {
+    this.cartService.addToCart({
+      product: newProduct.image,
+      name: newProduct.title,
+      price: newProduct.price,
+      quantity: 1,
+      id: newProduct.id
+    })
   }
 
 }
