bb1d35f7d261a0fc26dacd8b4791403361ed7a02
Viktor Lachkov <EPetrov2@gmail.com>
2023-10-12 15:32:17 +0400

    Add cart

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html'
+})
+export class CartComponent {
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 2,
+    id: 1
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers long name fdsdsf sdf sdfdsf sdfsd adsad sd',
+    price: 250,
+    quantity: 1,
+    id: 1
+  }]};
+
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ]
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map(item => item.quantity * item.price).
+    reduce((prev, current) => prev + current, 0)
+  }
+}
