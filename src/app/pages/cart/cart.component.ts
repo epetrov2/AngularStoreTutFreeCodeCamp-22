ecbd3332bf6e40b1462c5d279d24eb796bf799a1
Viktor Lachkov <EPetrov2@gmail.com>
2023-10-19 15:30:12 +0400

    Add cart items add and remove

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html'
 })
 export class CartComponent {
-  cart: Cart = { items: [{
-    product: 'https://via.placeholder.com/150',
-    name: 'snickers',
-    price: 150,
-    quantity: 2,
-    id: 1
-  },
-  {
-    product: 'https://via.placeholder.com/150',
-    name: 'snickers long name fdsdsf sdf sdfdsf sdfsd adsad sd',
-    price: 250,
-    quantity: 1,
-    id: 1
-  }]};
+  cart: Cart = {
+    items: [{
+      product: 'https://via.placeholder.com/150',
+      name: 'snickers',
+      price: 150,
+      quantity: 2,
+      id: 1
+    },
+    {
+      product: 'https://via.placeholder.com/150',
+      name: 'snickers long name fdsdsf sdf sdfdsf sdfsd adsad sd',
+      price: 250,
+      quantity: 1,
+      id: 1
+    }]
+  };
 
   dataSource: Array<CartItem> = [];
   displayedColumns: Array<string> = [
@@ -31,12 +34,31 @@ export class CartComponent {
     'action'
   ]
 
+  constructor(private cartService: CartService) { }
+
   ngOnInit(): void {
+    
+    this.cartService.cart.subscribe((_cart: Cart) => this.cart = _cart)
     this.dataSource = this.cart.items;
   }
 
   getTotal(items: Array<CartItem>): number {
-    return items.map(item => item.quantity * item.price).
-    reduce((prev, current) => prev + current, 0)
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -22,7 +22,41 @@ export class CartService {
     }
     this.cart.next({ items });
     this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
-    console.log(this.cart.value);
-    
+    //console.log(this.cart.value);
+
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map(item => item.quantity * item.price).
+      reduce((prev, current) => prev + current, 0)
+  }
+
+  clearCart(): void {
+    this.cart.next({ items: [] });
+    this._snackBar.open('Cart is cleared.', 'Ok', { duration: 3000 });
+  }
+
+  removeFromCart(item: CartItem): void {
+    const filteredItems = this.cart.value.items.filter((_item: CartItem) => _item.id !== item.id)
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open('Item removed from cart.', 'Ok', { duration: 3000 });
+  }
+
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined;
+    const filteredItems = this.cart.value.items.map((_item: CartItem) => {
+      if (_item.id === item.id) {
+        if (_item.quantity > 1) _item.quantity--;
+        else itemForRemoval = _item;
+      }
+
+      return _item;
+    })
+
+    if (itemForRemoval) this.removeFromCart(itemForRemoval)
+    else {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open('1 item removed from cart.', 'Ok', { duration: 3000 });
+    }
   }
 }
