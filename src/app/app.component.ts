bbcfbb3f6eb53f6ba3c6d45a88997c0fbf9a4f8f
Viktor Lachkov <EPetrov2@gmail.com>
2023-10-31 16:08:11 +0400

    Add cartSubscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Cart } from './models/cart.model';
 import { CartService } from './services/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,18 @@ import { CartService } from './services/cart.service';
   `,
   styles: []
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   cart: Cart = { items: []}
-
+  cartSubscription: Subscription | undefined;
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.cartService.cart.subscribe((_cart) => this.cart = _cart)
+    this.cartSubscription =  this.cartService.cart.subscribe((_cart) => this.cart = _cart);
+  }
+
+  ngOnDestroy(): void {
+    if (this.cartSubscription) {
+      this.cartSubscription.unsubscribe();
+    }
   }
 }
